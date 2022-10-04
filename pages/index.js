595844c8ab147576b91fc92d4086c5c4dd216820
G-Linares <gerrylinares@gmail.com>
2022-10-04 18:02:43 -0500

    ready for deployment

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import React from "react";
-import { Home as HomeComponent } from "../components";
+import { Home as HomeComponent } from "../Components";
 
 const Home = () => {
   return (
